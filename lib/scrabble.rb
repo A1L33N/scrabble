89cce09b510b25dcfd0049531d8da8dcbff2cdfa
Ronando Long & Aileen Cacayorin <student@epicodus.com>
2015-08-04 14:01:22 -0700

    return an array that contains the score for each letter of a word

---
+++ lib/scrabble.rb
@@ -0,0 +1,39 @@
+class String
+  define_method(:scrabble) do
+
+    key_name = {
+      'a'=> 1,
+      'e'=> 1,
+      'r'=> 1,
+      's'=> 1,
+      't'=> 1,
+      'u'=> 1,
+      'i'=> 1,
+      'l'=> 1,
+      'n'=> 1,
+      'o'=> 1,
+      'd'=> 2,
+      'g'=> 2,
+      'b'=> 3,
+      'c'=> 3,
+      'f'=> 4,
+      'h'=> 4,
+      'j'=> 8,
+      'k'=> 5,
+      'm'=> 3,
+      'p'=> 3,
+      'q'=> 10,
+      'v'=> 4,
+      'w'=> 4,
+      'x'=> 8,
+      'y'=> 4,
+      'z'=> 10
+      }
+    word_array = self.split("")
+    score_array = []
+    word_array.each() do |letter|
+      score_array.push(key_name.fetch(letter))
+    end
+      score_array
+  end
+end
---
+++ spec/scrabble_spec.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('scrabble')
+
+
+describe ("String#scrabble") do
+  it("returns a scrabble score for a letter") do
+  expect("a".scrabble()).to(eq([1]))
+  end
+
+  it("returns an array that contains the score for each letter of a word") do
+    expect(("car").scrabble()).to(eq([3, 1, 1]))
+  end
+end
