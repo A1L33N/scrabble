cf8aabc7f6f8b2886b0899ac602a2e44030d62ee
Guest User <student@epicodus.com>
2015-08-05 11:49:52 -0700

    Complete Scrabble score generator.

--- spec/scrabble_integration_spec.rb
+++ spec/scrabble_integration_spec.rb
@@ -2,7 +2,7 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 
-describe('the scrabble path'), {:type => feature}) do
+describe('the scrabble path', {:type => :feature}) do
   it('processes a word entry and returns a scrabble score') do
     visit('/')
     fill_in('word', :with => 'apple')
